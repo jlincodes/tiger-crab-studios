14114c7f733fbc8fabe97bb10f5ee721e00fc036
Julie <julie.lin0812@gmail.com>
2018-01-17 15:11:50 -0800

    Sets up project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  },
+};
