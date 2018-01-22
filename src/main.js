0922f00601f50ff5d6b190a77468379a2c78b1d7
Julie <julie.lin0812@gmail.com>
2018-01-21 17:13:35 -0800

    Imports images and adds styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './css/main.css';
+import Blog from './images/blog.jpg';
+import App from './images/nevera_app.jpg';
+import CardGame from './images/nevera_card_game.jpg';
+import Comic from './images/nevera_comic.jpg';
+import Novel from './images/nevera_novel.jpg';
+import VideoGame from './images/nevera_video_game.jpg';
 
 export default class Main extends React.Component {
   render() {
@@ -29,8 +35,165 @@ export default class Main extends React.Component {
                 We are working on video games, board games, comic books and more. Currently all our projects are set in the fantasy horror genre and produced with the power of Kickstarter.
               </p>
             </div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/JgnWTpQ9Zw0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="560" height="315" title="The White Room" src="https://www.youtube.com/embed/JgnWTpQ9Zw0" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           </div>
+        </section>
+
+        <hr></hr>
+
+        <section id='dev-blog'>
+          <h2>Development Blog</h2>
+          <hr></hr>
+          <div className="dev-blog">
+            <div className="img-wrapper blog-img">
+              <a href="http://www.tigercrabstudiosblog.com/">
+                <img src={Blog} alt='blog'></img>
+              </a>
+            </div>
+            <div className="desc-text-wrapper">
+              <a href="http://www.tigercrabstudiosblog.com/">
+                <h3>Tiger Crab Studios Blog</h3>
+              </a>
+              <p>
+                Click here to visit the Tiger Crab Studios Development Blog, full of useful advice and stories behind the creation of the projects.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section id='projects'>
+          <h2>Projects</h2>
+
+          <hr></hr>
+
+          <div className="project">
+            <div className="desc-text-wrapper">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-wars-card-game">
+                <h3>Nevera Wars <i>Card Game</i></h3>
+              </a>
+              <p>
+                Nevera Wars is a deck construction card game where players create their own deck of undead minions.
+              </p>
+            </div>
+            <div className="img-wrapper card-game-img">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-wars-card-game">
+                <img src={CardGame} alt='card-game'></img>
+              </a>
+            </div>
+          </div>
+
+          <hr></hr>
+
+          <div className="project">
+            <div className="img-wrapper comic-img">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-tales-comic-book">
+                <img src={Comic} alt='comic'></img>
+              </a>
+            </div>
+            <div className="desc-text-wrapper">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-tales-comic-book">
+                <h3>Nevera Tales <i>Comic Book</i></h3>
+              </a>
+              <p>
+                Explore a more intimate side of Nevera as the Nevera Tales comic books.
+              </p>
+            </div>
+          </div>
+
+          <hr></hr>
+
+          <div className="project">
+            <div className="desc-text-wrapper">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-wars-app-game">
+                <h3>Nevera Wars <i>App Game</i></h3>
+              </a>
+              <p>
+                Nevera Wars will also be published as an app game and web game to allow more people to experience the game before deciding to purchase a physical copy. There will be a campaign mode and online multiplayer.
+              </p>
+            </div>
+            <div className="img-wrapper app-img">
+              <img src={App} alt="app"></img>
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-wars-app-game">
+              </a>
+            </div>
+          </div>
+
+          <hr></hr>
+
+          <div className="project">
+            <div className="img-wrapper video-game-img">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-legends-video-game">
+                <img src={VideoGame} alt="video-game"></img>
+              </a>
+            </div>
+            <div className="desc-text-wrapper">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-legends-video-game">
+                <h3>Nevera Legends <i>Video Game</i></h3>
+              </a>
+              <p>
+                Nevera Legends is a monster collection RPG, players will travel across the world of Nevera in search of restoring their friend back to full life after a resurrection was performed catastrophically. But will the power to undo the mistake be worth the price?
+              </p>
+            </div>
+          </div>
+
+          <hr></hr>
+
+          <div className="project">
+            <div className="desc-text-wrapper">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-legends-novel">
+                <h3>Nevera Legends <i>Novel</i></h3>
+              </a>
+              <p>
+                Project description
+              </p>
+            </div>
+            <div className="img-wrapper novel-img">
+              <a href="https://sites.google.com/view/tigercrabstudios/nevera-legends-novel">
+                <img src={Novel} alt="novel"></img>
+              </a>
+            </div>
+          </div>
+
+          <hr></hr>
+        </section>
+
+        <section id='follow-us'>
+          <h2>Follow Us</h2>
+
+          <hr></hr>
+
+          <div className="social-links">
+            <a href="https://www.facebook.com/tigercrabstudios/">
+              <i className="fab fa-facebook-square fa-3x"></i>
+            </a>
+            <a href="https://twitter.com/tigercrabstudio">
+              <i className="fab fa-twitter fa-3x"></i>
+            </a>
+            <a href="https://tigercrabstudios.tumblr.com/">
+              <i className="fab fa-tumblr-square fa-3x"></i>
+            </a>
+            <a href="https://plus.google.com/107667168963867360033">
+              <i className="fab fa-google-plus-g fa-3x"></i>
+            </a>
+            <a href="https://www.instagram.com/tigercrabstudios/">
+              <i className="fab fa-instagram fa-3x"></i>
+            </a>
+            <a href="https://www.reddit.com/r/TigerCrabStudios/">
+              <i className="fab fa-reddit-alien fa-3x"></i>
+            </a>
+            <a href="https://www.kickstarter.com/profile/563979601">
+              <i className="fab fa-kickstarter-k fa-3x"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCBOkmrgNF0GbBrpd4lXJYWg">
+              <i className="fab fa-youtube fa-3x"></i>
+            </a>
+            <a href="https://gumroad.com/tigercrabstudios">
+              <i className="fas fa-shopping-cart fa-3x"></i>
+            </a>
+          </div>
+
         </section>
       </div>
     );
